a6b8a600c4d9c2313d4fddb109320674eca103f9
MFV-Felix <88993841+MWV-Felix@users.noreply.github.com>
2025-02-21 20:16:08 +0700

    remove telescope plugin

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,15 +1,32 @@
 return {
-  {
-    "tpope/vim-rails",
-    config = function()
-      vim.g.rails_app_root = vim.fn.getcwd()
-    end
-  },
-  {
-    "slim-template/vim-slim",
-    ft = "slim",
-    config = function()
-      vim.g.slim_indent_tags = "html,body,head,meta,link,script,style,link_to,form_for,form_tag"
-    end
-  }
+	{
+		"tpope/vim-rails",
+		config = function()
+			vim.g.rails_app_root = vim.fn.getcwd()
+		end,
+	},
+	{
+		"slim-template/vim-slim",
+		ft = "slim",
+		config = function()
+			vim.g.slim_indent_tags = "html,body,head,meta,link,script,style,link_to,form_for,form_tag"
+		end,
+	},
+	{
+		"github/copilot.vim",
+	},
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		branch = "main",
+		cmd = { "CopilotChat" },
+		dependencies = {
+			{ "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+			{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+		},
+		build = "make tiktoken", -- Only on MacOS or Linux
+		opts = {
+			debug = true, -- Enable debugging
+			-- See Configuration section for rest
+		},
+	},
 }
